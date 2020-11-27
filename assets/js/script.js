ef0bf6aa1624d2e3815010d5b95cbb666bcf227e
sanjayns404 <sanjayns404@gmail.com>
2020-11-27 05:25:31 -0800

    Final Touch

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,29 @@ $('.card').mouseenter(function(){
         $(this).find('.card-hover').css({'top':'100%'});
 });
 
-$(window).load(function() {
-	$(".loader").delay(500).fadeOut("slow");
-  $("#overlayer").delay(500).fadeOut("slow");
-})
+function autoResizeDiv()
+        {
+            document.getElementById('home').style.height = window.innerHeight  +'px';            
+        }
+        window.onresize = autoResizeDiv;
+        autoResizeDiv();
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml3');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml3 .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 800,
+    delay: (el, i) => 150 * (i+1)
+  }).add({
+    targets: '.ml3',
+    opacity: 0,
+    duration: 500,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
